464b2a99acd82bc1e7bb90ba910268e8661618d0
ronesta <gvozzzd123@gmail.com>
2024-12-28 23:07:57 +0300

    Initial commit

---
+++ RMUserDefaultsMVC/RMUserDefaultsMVC/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RMUserDefaultsMVC
+//
+//  Created by Ибрагим Габибли on 28.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
