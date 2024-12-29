ccbe4fea80b44908886427f631dc6603f86d493d
ronesta <gvozzzd123@gmail.com>
2024-12-29 14:00:55 +0300

    Project without architecture

---
+++ RMUserDefaultsMVC/RMUserDefaultsMVC/Character.swift
@@ -0,0 +1,25 @@
+//
+//  Character.swift
+//  RMUserDefaultsMVC
+//
+//  Created by Ибрагим Габибли on 29.12.2024.
+//
+
+import Foundation
+
+struct PostCharacters: Codable {
+    let results: [Character]
+}
+
+struct Character: Codable {
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let location: Location
+    let image: String
+}
+
+struct Location: Codable {
+    let name: String
+}
